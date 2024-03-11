8b039d7ba20475100c7bc096e58dcfd8de43970e
Викторов Павел <decidedtosolvethisproblem@gmail.com>
2024-03-11 09:45:33 +0300

    скрипты

---
+++ SqlScripts/CreateSequence_CountryIdSequence.sql
@@ -0,0 +1,17 @@
+USE [IServApp]
+GO
+
+USE [IServApp]
+GO
+
+/****** Object:  Sequence [dbo].[CountryIdSequence]    Script Date: 11.03.2024 9:41:52 ******/
+CREATE SEQUENCE [dbo].[CountryIdSequence] 
+ AS [int]
+ START WITH 1
+ INCREMENT BY 1
+ MINVALUE 1
+ MAXVALUE 100000
+ CACHE 
+GO
+
+
---
+++ SqlScripts/CreateTable_Country.sql
@@ -0,0 +1,24 @@
+USE [IServApp]
+GO
+
+/****** Object:  Table [dbo].[Country]    Script Date: 11.03.2024 9:40:57 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Country](
+	[CountryId] [int] NOT NULL,
+	[Name] [nvarchar](max) NOT NULL,
+ CONSTRAINT [PK_Country] PRIMARY KEY CLUSTERED 
+(
+	[CountryId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Country] ADD  DEFAULT (NEXT VALUE FOR [CountryIdSequence]) FOR [CountryId]
+GO
+
+
